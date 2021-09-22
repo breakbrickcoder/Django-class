1356f7ec368f47863cafe464e68067023669cc35
breakbrickcoder <davidgurung826@gmail.com>
2021-09-22 11:25:32 +0545

    first commit

---
+++ home/urls.py
@@ -0,0 +1,16 @@
+from django.urls import path
+from .views import *
+
+app_name = "home"
+
+
+urlpatterns = [
+	path('',home,name = 'home'),
+
+	path('about',about,name = 'about'),
+	path('contact',contact,name = 'contact'),
+	path('elements',elements,name = 'elements'),
+	path('portfolio',portfolio,name = 'portfolio'),
+	path('price',price,name = 'price'),
+	path('services',services,name = 'services'),
+]
